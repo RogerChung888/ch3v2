6b09e2f488522faa4bdc2f83910c83e39cc87d3a
RogerChung888 <108163951+RogerChung888@users.noreply.github.com>
2022-06-27 16:53:09 +0800

    Add files via upload

---
+++ ch3.py
@@ -0,0 +1,22 @@
+momo = input('請輸入密碼才可以看機密')
+
+if momo=='ch3':
+	print('rock_paper_scissors')
+	print('1 nineteen,twenty.are you ready')
+	print('2 not yet. wait')
+	print('3 hurry up')
+	print('4 ok,5ok')
+	print('6 where ate you,Niki')
+	print('7 is m in the bedroom')
+	print('8 a-ha i see you.hmm....where is Danny')
+	print('9 he is in the dining room')
+	print('10 where are you Danny')
+	print('11 here')
+	print('12 ha there you are')
+	print('13 uh-oh')
+	print('14 it is your turn')
+	print('15 where are you,Dino')
+	print('16 look he is sleeping')
+if momo!='ch3':
+
+          print('你打錯了 請重新輸入')
